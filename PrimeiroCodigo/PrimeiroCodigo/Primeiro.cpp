f54c0b2937df9b2a43d6cbda56a8a033211fabae
Mauricio Gonzatto <mauriciogonzatto@hotmail.com>
2022-03-03 09:51:39 -0300

    Add project files.

---
+++ PrimeiroCodigo/PrimeiroCodigo/Primeiro.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+	/*
+	std::cout << "Primeiro Codigo" << std::endl;
+	std::cout << "Segundo Codigo" << std::endl;
+	system("color 5F");
+	*/
+
+	std::cout << "EU\nVOU\nAPRENDER\nC++" << std::endl;
+
+	system("PAUSE");
+	return 0;
+}
