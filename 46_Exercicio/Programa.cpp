ab5ea85dbe4fa45e50aa0cf83d512c3eef90e218
Mauricio Gonzatto <mauriciogonzatto@hotmail.com>
2022-03-03 13:40:41 -0300

    Loop de controle de saída

--- 46_Exercicio/Programa.cpp
+++ 46_Exercicio/Programa.cpp
@@ -6,18 +6,28 @@ int main()
 
 	float Num1, Num2, Num3;
 	float Media{ 0.0 };
+	char Resposta = 'n';
 
-	std::cout << "Digite o Primeiro Número: ";
-	std::cin >> Num1;
-	std::cout << "Digite o Segundo Número: ";
-	std::cin >> Num2;
-	std::cout << "Digite o Terceiro Número: ";
-	std::cin >> Num3;
+	do
+	{
+		std::cout << "Digite o Primeiro Número: ";
+		std::cin >> Num1;
+		std::cout << "Digite o Segundo Número: ";
+		std::cin >> Num2;
+		std::cout << "Digite o Terceiro Número: ";
+		std::cin >> Num3;
 
-	Media = (Num1 + Num2 + Num3) / 3;
+		Media = (Num1 + Num2 + Num3) / 3;
 
-	std::cout << "Números digitados: " << Num1 << " " << Num2 << " " << Num3 << "\n";
-	std::cout << "Média dos número: " << Media << "\n";
+		std::cout << "Números digitados: " << Num1 << " " << Num2 << " " << Num3 << "\n";
+		std::cout << "Média dos número: " << Media << "\n";
+
+		std::cout << "\nDeseja calcular outra Média Artmética? (Responda S para Sim e N para não:) ";
+		std::cin >> Resposta;
+
+	} while (Resposta == 'S' || Resposta == 's');
+
+
 
 	system("PAUSE");
 	return 0;
