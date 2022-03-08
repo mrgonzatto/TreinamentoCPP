13edd1e0e40caaef0129f011496ea6158eb72c03
Mauricio Gonzatto <mauriciogonzatto@hotmail.com>
2022-03-08 14:21:49 -0300

    Ponteiros genéricos

---
+++ 101_PonteiroGenerico/PonteiroGenerico.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main()
+{
+	int Numero{ 4890 };
+	char Letra{ 'E' };
+
+	// Ponteiro vazio, não possui ainda um tipo de referência
+	// Útil para retornar algo de uma função e atribuir a um ponteiro
+	// assim não precisamos saber antecipadamente que tipo de dado retorna	
+	void* ptrG;
+	// ptrg agora aponta para a variável Letra
+	ptrG = &Letra;
+
+	// A instrução abaixo acusa erro pois ptrG ainda não conhece o tipo e não sabe
+	// como ler a variável pois não calcula a quantidade de memória a ser percorrida
+	// em bytes, só saberá após a indicação do tipo
+	//std::cout << "Valor de letra via ptrG = " << *ptrG;
+
+	// Primeiro faz o cast do ponteiro e depois aplica o * para realizar o 
+	// desreferenciamento
+	std::cout << "Valor de Letra via ptrG = " << *(char*)ptrG << "\n";
+
+	ptrG = &Numero;
+	std::cout << "Valor de Numero via ptrG:" << *(int*)ptrG << "\n";
+
+	system("PAUSE");
+	return 0;
+}
--- 99_ConsolidandoPonteiros/ConsolidandoPonteiros.cpp
+++ 99_ConsolidandoPonteiros/ConsolidandoPonteiros.cpp
@@ -4,9 +4,20 @@ int main()
 {
 	setlocale(LC_ALL, "portuguese");
 
-	int Numero{ 102 };
+	int Numero{ 4096 };
 	int* ptr = &Numero;
+	int* ptr2 = ptr;
 
+	std::cout << "Endereço contido em ptr: " << ptr << "\n";
+	std::cout << "Endereço contido em ptr2: " << ptr2 << "\n";
+
+	std::cout << "Endereço de ptr na memória RAM: " << &ptr << "\n";
+	std::cout << "Endereço de ptr2 na memória RAM: " << &ptr2 << "\n";
+
+	*ptr2 = (*ptr + 10);
+	std::cout << "\nValor de Numero agora: " << Numero << "\n";
+
+	/*
 	std::cout << "\nValor da Variável Número: " << Numero << "\n";
 	std::cout << "\nValor da Variável ptr: " << ptr << "\n";
 	std::cout << "\nValor do Endereço de Número: " << &Numero << "\n";
@@ -14,6 +25,7 @@ int main()
 	std::cout << "\nValor contido na variável apontada por ptr: " << *ptr << "\n";
 	*ptr = 20963;
 	std::cout << "\nValor contido na variável apontada por ptr: " << *ptr << "\n";
+	*/
 
 	system("PAUSE");
 	return 0;
